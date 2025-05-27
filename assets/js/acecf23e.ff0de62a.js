7f4d1b1aaa7962999df15eeda0370f6c8b868bd6
Lesly Davilmar <leslybydavilmar@gmail.com>
2025-05-27 20:32:00 +0200

    Deploy website - based on 7dc575413fc9a770ea62096bdb8bd7510ea47026

---
+++ assets/js/acecf23e.ff0de62a.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunksmartreception_doc=self.webpackChunksmartreception_doc||[]).push([[1903],{1912:o=>{o.exports=JSON.parse('{"blogBasePath":"/FrenchVox-docs/blog","blogTitle":"Blog","authorsListPath":"/FrenchVox-docs/blog/authors"}')}}]);
